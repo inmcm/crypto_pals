f366b2524f69d37698488c6657f5cfe8ccbe6cbd
Calvin McCoy <calvin.mccoy@gmail.com>
2016-10-01 00:52:57 -0500

    Make Older Solutions Importable
    Also improve solution to task three a bit

--- Set_1_Basics/task3.py
+++ Set_1_Basics/task3.py
@@ -8,19 +8,20 @@ freqs = {'a': 6.51738, 'b': 1.24248, 'c': 2.17339, 'd': 3.49835, 'e': 0.41442,
          'u': 2.25134, 'v': 0.82903, 'w': 1.71272, 'x': 0.13692, 'y': 1.45984,
          'z': 0.07836, ' ': 19.18182}
 
-test = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-sample_bytes = unhexlify(test)
-best = (0,0,0)
-for xor_key in range(256):
-    test_bytes = bytes([xor_key ^ c for c in sample_bytes])
-    total_chars = sum([test_bytes.count(x) for x in string.printable.encode()])
-    if total_chars == len(test_bytes):
-        test_string = test_bytes.decode()
-        score = sum([freqs[x] for x in test_string if x in freqs])
-        if score > best[1]:
-            best = (xor_key, score, test_string)
+if __name__ == '__main__':
+    test = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    sample_bytes = unhexlify(test)
+    best = (0,0,0)
+    for xor_key in range(256):
+        test_bytes = bytes([xor_key ^ c for c in sample_bytes])
+        total_chars = sum([test_bytes.count(x) for x in string.printable.encode()])
+        if total_chars == len(test_bytes):
+            test_string = test_bytes.decode()
+            score = sum([freqs[x] for x in test_string.lower() if x in freqs])
+            if score > best[1]:
+                best = (xor_key, score, test_string)
 
-    
-print("Best XOR Key:", hex(best[0]))
-print("Score:",best[1])
-print("Decoded String:",best[2])
+        
+    print("Best XOR Key:", hex(best[0]))
+    print("Score:",best[1])
+    print("Decoded String:",best[2])
--- Set_1_Basics/task5.py
+++ Set_1_Basics/task5.py
@@ -11,16 +11,16 @@ class key_byte_gen:
         self.count += 1
         return key_byte
 
+if __name__ == '__main__':  
+    p = key_byte_gen('ICE')
+    input_string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    input_bytes = input_string.encode()
 
-p = key_byte_gen('ICE')
-input_string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-input_bytes = input_string.encode()
-
-xor_bytes = bytes([p.next() ^ x for x in input_bytes])
-print(hexlify(xor_bytes))
-solution = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
-solution_bytes = unhexlify(solution)
-p = key_byte_gen('ICE')
-f = bytes([p.next() ^ x for x in solution_bytes])
-print(f.decode())
-assert f.decode() == input_string
+    xor_bytes = bytes([p.next() ^ x for x in input_bytes])
+    print(hexlify(xor_bytes))
+    solution = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+    solution_bytes = unhexlify(solution)
+    p = key_byte_gen('ICE')
+    f = bytes([p.next() ^ x for x in solution_bytes])
+    print(f.decode())
+    assert f.decode() == input_string
