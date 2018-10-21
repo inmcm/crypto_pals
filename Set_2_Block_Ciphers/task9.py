12b093d1f7361afb7ecd56daec49b594b361b5b9
Calvin McCoy <calvin.mccoy@gmail.com>
2018-10-21 12:58:15 -0700

    Fix padding function

--- Set_2_Block_Ciphers/task9.py
+++ Set_2_Block_Ciphers/task9.py
@@ -1,11 +1,10 @@
 
 
 def pad_bytes_size(in_bytes, block_size):
-    if len(in_bytes) > block_size:
-        pad_size = block_size - (len(in_bytes) % block_size) 
-    else:
-        pad_size = block_size % len(in_bytes)
-    
+    input_len = len(in_bytes)
+    if (input_len % block_size) == 0:
+        return in_bytes
+    pad_size = block_size - (input_len % block_size)
     padded_bytes = in_bytes + bytes([pad_size for _ in range(pad_size)])
     return padded_bytes
 
@@ -13,6 +12,13 @@ def pad_bytes_size(in_bytes, block_size):
 if __name__ == '__main__':
     test_bytes = b'YELLOW SUBMARINE'
     block_size = 20
+    padded = pad_bytes_size(test_bytes, block_size)
+    print('Block Size:', block_size)
+    print('Before:', test_bytes)
+    print('After:', padded)
+
+    test_bytes = b'A'
+    block_size = 16
     padded = pad_bytes_size(test_bytes, block_size)
     print('Block Size:',block_size)
     print('Before:',test_bytes)
